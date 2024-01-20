576f2077d12f1443bd6277a3875c7088dc5ad3f4
unknown <sunalichandrasekaran>
2024-01-20 16:05:21 +0530

    first commi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Post from "./Components/Post";
+import { BrowserRouter, Link, Route, Router, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <ul>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/contact">
+            <li>Contact</li>
+          </Link>
+          <Link to="/post/1">
+            <li>Post</li>
+          </Link>
+          <Link to="/post/2">
+            <li>Post2</li>
+          </Link>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/post/:id" element={<Post />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
